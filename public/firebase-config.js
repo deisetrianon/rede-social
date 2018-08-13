3d259e72b62dbef89385aaa9dfe29588e5345589
Deise Trianon <deisetrianon@gmail.com>
2018-08-13 19:31:55 -0300

    apenas testes

---
+++ public/firebase-config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAuC4qiPqqISpnkw_23Z1MqanztxWrsnHE",
+  authDomain: "rede-social-9136e.firebaseapp.com",
+  databaseURL: "https://rede-social-9136e.firebaseio.com",
+  projectId: "rede-social-9136e",
+  storageBucket: "rede-social-9136e.appspot.com",
+  messagingSenderId: "1092006292153"
+};
+firebase.initializeApp(config);
